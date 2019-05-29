a4afa322a010980a268f2187a52bd9353f457dfc
ramin <rostami.git@gmail.com>
2019-05-29 05:06:00 -0400

    Create GET /chats route

---
+++ index.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import {chats} from './db'
+import { resolve } from 'path';
+const app = express()
+
+app.get('/_ping', (req, res) => {
+    res.send("pong");
+})
+
+app.get('/chats', (req, res) => {
+    res.json(chats)
+})
+
+const port = process.env.PORT || 4000 
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
+
